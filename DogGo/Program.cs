a984831d285dee93ed95e43ddaf60a04f5e15fbc
Jeremy White <jcwhite414@gmail.com>
2023-04-03 21:04:48 -0500

    Dependency injection for Owners controller

--- DogGo/Program.cs
+++ DogGo/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddTransient<IWalkerRepository, WalkerRepository>();
+builder.Services.AddTransient<IOwnerRepository, OwnerRepository>();
 
 var app = builder.Build();
 
